6045c2b195739bc5a48438111ea5f3868a541190
Gigg <danya19981017@gmail.com>
2024-06-26 21:59:51 +0200

    Updated

---
+++ Controllers/CalorieCalculatorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalorieCountingApp.Controllers
+{
+    public class CalorieCalculatorController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult WeightLoss() {
+            return View();
+        }
+
+        public IActionResult WeightGain() {
+            return View();
+        }
+
+        public IActionResult WeightMaintenance() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CalculateCalories(string goal, double weight, double height, int age, string gender) {
+            double bmr = 0;
+
+            if (gender == "Male") {
+                bmr = 10 * weight + 6.25 * height - 5 * age + 5;
+            }
+            else if (gender == "Female") {
+                bmr = 10 * weight + 6.25 * height - 5 * age - 161;
+            }
+
+            double dailyCalories = bmr;
+
+            switch (goal) {
+                case "WeightLoss":
+                    dailyCalories *= 0.8;
+                    break;
+
+                case "WeightGain":
+                    dailyCalories *= 1.2;
+                    break;
+
+                case "WeightMaintenance":
+                    break;
+            }
+
+            ViewBag.DailyCalories = dailyCalories;
+            return View("Result");
+        }
+    }
+}
---
+++ Controllers/SumCalorieCalculatorController.cs
@@ -0,0 +1,57 @@
+﻿using CalorieCountingApp.Data;
+using CalorieCountingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalorieCountingApp.Controllers
+{
+    public class SumCalorieCalculatorController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SumCalorieCalculatorController(AppDbContext context) {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string searchString) {
+            ViewBag.CurrentFilter = searchString;
+            var foodItems = from f in _context.FoodItems select f;
+
+            if (!string.IsNullOrEmpty(searchString)) {
+                foodItems = foodItems.Where(f => f.Name.Contains(searchString));
+            }
+            return View(await foodItems.AsNoTracking().ToListAsync());
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(int[] foodItemIds, int[] weights) {
+            double totalCalories = 0;
+            double totalFats = 0;
+            double totalProteins = 0;
+            List<FoodItem> selectedItems = new List<FoodItem>();
+
+            for (int i = 0; i < foodItemIds.Length; i++) {
+                var foodItem = _context.FoodItems.Find(foodItemIds[i]);
+                if (foodItem != null) {
+                    totalCalories += (foodItem.Calories / 100.0) * weights[i];
+                    totalFats += (foodItem.Fats / 100.0) * weights[i];
+                    totalProteins += (foodItem.Proteins / 100.0) * weights[i];
+                    selectedItems.Add(new FoodItem {
+                        Id = foodItem.Id,
+                        Name = foodItem.Name,
+                        Calories = foodItem.Calories,
+                        Fats = foodItem.Fats,
+                        Proteins = foodItem.Proteins
+                    });
+                }
+            }
+
+            ViewBag.TotalCalories = totalCalories;
+            ViewBag.TotalFats = totalFats;
+            ViewBag.TotalProteins = totalProteins;
+            ViewBag.SelectedItems = selectedItems;
+
+            return View("Result");
+        }
+    }
+}
